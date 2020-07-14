6a8c517c78c3a0478016beb57f4616b8dd0b5a90
svikramjeet <vikramjeet@ucreate.co.in>
2020-07-14 12:31:35 +0530

    Created custom component
    1. Moved all links from homepage to custom component with name HomeUrlComponent
    2. Updated public/js/app.js with npm run watch
    3. Removed hello component call from welcome.blade.php

---
+++ resources/js/components/HomeUrlComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,18 +83,10 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-                {{-- Example Vue Component --}}
-                <example-component></example-component>
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                
+                {{-- HomeUrl Vue Component --}}
+                <homepage-links></homepage-links>
+                
             </div>
         </div>
     </body>
